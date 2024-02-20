de92c388a03507374a98e94d1d87b47e6bd3aac3
AlonsoMartinez8 <alumno.574777@ies-azarquiel.es>
2024-02-20 19:17:27 +0100

    Tabs design

--- src/screens/MyMenu.jsx
+++ src/screens/MyMenu.jsx
@@ -1,15 +1,63 @@
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
-import PersonajesScreen from "./PersonajesScreen"
-import MundosScreen from "./MundosScreen"
-import FavoritosScreen from "./FavoritosScreen"
-const Tab = createBottomTabNavigator()
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import PersonajesScreen from "./PersonajesScreen";
+import MundosScreen from "./MundosScreen";
+import FavoritosScreen from "./FavoritosScreen";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+const Tab = createBottomTabNavigator();
 
 export default function MyMenu() {
   return (
-    <Tab.Navigator>
-        <Tab.Screen name="Personajes" component={PersonajesScreen}/>
-        <Tab.Screen name="Mundos" component={MundosScreen}/>
-        <Tab.Screen name="Favoritos" component={FavoritosScreen}/>
+    <Tab.Navigator
+      screenOptions={{
+        tabBarActiveTintColor: "#ff9711",
+        tabBarActiveBackgroundColor: "#000",
+      }}
+    >
+      <Tab.Screen
+        name="Personajes"
+        component={PersonajesScreen}
+        options={{
+          tabBarIcon: ({ size, color }) => {
+            return (
+              <MaterialCommunityIcons
+                name="account"
+                size={size}
+                color={color}
+              />
+            );
+          },
+        }}
+      />
+      <Tab.Screen
+        name="Mundos"
+        component={MundosScreen}
+        options={{
+          tabBarIcon: ({ size, color }) => {
+            return (
+              <MaterialCommunityIcons
+                name="earth"
+                size={size}
+                color={color}
+              />
+            );
+          },
+        }}
+      />
+      <Tab.Screen
+        name="Favoritos"
+        component={FavoritosScreen}
+        options={{
+          tabBarIcon: ({ size, color }) => {
+            return (
+              <MaterialCommunityIcons
+                name="progress-star"
+                size={size}
+                color={color}
+              />
+            );
+          },
+        }}
+      />
     </Tab.Navigator>
-  )
+  );
 }
