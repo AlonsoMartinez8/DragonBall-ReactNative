910ac96c5051e979a90fac3babbce48576f0d431
Alejandro Sosa Fernández <alejandrosf913@gmail.com>
2024-02-21 12:12:07 +0100

    stack mundo2

---
+++ src/navigation/StackMundo.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import MundosScreen from '../screens/MundosScreen';
+import MundoDetails from '../screens/MundoDetails';
+
+const MundoStack = createNativeStackNavigator();
+
+export default function StackMundor() {
+  return (
+    <MundoStack.Navigator>
+      <MundoStack.Screen
+        name="MundosScreen"
+        component={MundosScreen}
+      />
+      <MundoStack.Screen
+        name="MundoDetails"
+        component={MundoDetails}
+      />
+
+
+    </MundoStack.Navigator>
+  )
+}
