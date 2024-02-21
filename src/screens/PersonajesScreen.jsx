930456464760a2ceb8efbdf8c5191ba9c61b0f2a
AlonsoMartinez8 <alumno.574777@ies-azarquiel.es>
2024-02-21 10:37:48 +0100

    getCharactersByPage

--- src/screens/PersonajesScreen.jsx
+++ src/screens/PersonajesScreen.jsx
@@ -1,9 +1,41 @@
-import { View, Text } from "react-native";
+import { useEffect, useState } from "react";
+import { View, Text, FlatList } from "react-native";
+import { getCharactersByPage } from "../services/dragonBallAPI";
 
 export default function PersonajesScreen() {
+  const [personajes, setPersonajes] = useState([])
+  const [paginaActual, setPaginaActual] = useState(1)
+  const [paginasTotales, setPaginasTotales] = useState(0)
+
+  const getPersonajes = (page=1)=>{
+    getCharactersByPage(page)
+    .then(json=>{
+      setPersonajes(previos=>[...previos, ...json.items])
+      setPaginasTotales(json.meta.totalPages)
+      setPaginaActual(json.meta.currentPage)
+    })
+    .catch(error=>console.log(error))
+  }
+
+  useEffect(() => {
+    getPersonajes();
+  }, []);
+
   return (
     <View>
-        <Text>Personajes</Text>
+      <FlatList
+        data={personajes}
+        renderItem={({ item }) => (
+          <Text key={item.id}>{item.name}</Text>
+        )}
+        onEndReachedThreshold={0}
+        onEndReached={() => {
+          if (paginaActual < paginasTotales) {
+            getPersonajes(paginaActual + 1);
+            setPaginaActual(paginaActual + 1);
+          }
+        }}
+      />
     </View>
-  )
-}
+  );
+};
--- src/services/dragonBallAPI.js
+++ src/services/dragonBallAPI.js
@@ -1,7 +1,7 @@
-const API = "https://www.dragonball-api.com/api/";
+const API = "https://dragonball-api.com/api/";
 
-export async function getAllCharacters() {
-  const data = await fetch(`${API}characters`);
+export async function getCharactersByPage(page = 1) {
+  const data = await fetch(`${API}characters?page=${page}`);
   const json = await data.json();
   return json;
 }
