6efeafd4bff88cf6c58c04fa5b772c1467d65202
Sufi <116170704+SufiAzarquiel@users.noreply.github.com>
2024-02-21 13:57:31 +0100

    update mundo card

--- src/components/MundoCard.jsx
+++ src/components/MundoCard.jsx
@@ -1,34 +1,42 @@
 import React from 'react'
-import { StyleSheet, View, Image, Text } from 'react-native'
+import { StyleSheet, View, Image, Text, Dimensions } from 'react-native'
+
+let windowWidth = Dimensions.get('window').width;
 
 export default function MundoCard({ item }) {
   return (
-    <View style={styles.row}>
-      <Image source={{ uri: item.image }} style={styles.image} resizeMode="contain" />
-      <View style={[styles.column, { marginLeft: 10 }]}>
-        <Text style={[styles.text, { fontWeight: "bold" }]}>{item.name}</Text>
-        <Text style={styles.text}>{item.description}</Text>
+    <View style={styles.card}>
+      <Image source={{ uri: item.image }} style={styles.image} resizeMode="cover" />
+      <View style={styles.overlay}>
+        <Text style={styles.text}>{item.name}</Text>
       </View>
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
+  card: {
+    width: '90%',
+    height: 400,
+    marginHorizontal: 20,
+    marginVertical: 10,
+    borderRadius: 15, // Adjust the border radius as needed
+    overflow: 'hidden',
+  },
   image: {
-    width: 80,
-    height: 80
-  },
-  row: {
     flex: 1,
-    flexDirection: "row",
-    margin: 10
+    width: '100%',
+    height: '100%',
   },
-  column: {
-    flex: 1,
-    flexDirection: "column",
-    justifyContent: "flex-start"
+  overlay: {
+    ...StyleSheet.absoluteFillObject,
+    backgroundColor: 'rgba(0, 0, 0, 0.6)',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   text: {
-    fontSize: 18
-  }
+    fontSize: 36,
+    color: 'white',
+    fontWeight: 'bold',
+  },
 });
