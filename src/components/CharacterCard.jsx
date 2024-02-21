106e36e6196af9c20bca19fcbd18a9a4c263e833
AlonsoMartinez8 <alumno.574777@ies-azarquiel.es>
2024-02-21 12:04:59 +0100

    Image Character Card

--- src/components/CharacterCard.jsx
+++ src/components/CharacterCard.jsx
@@ -1,10 +1,31 @@
-import { Image, Text, View } from "react-native";
+import { Image, StyleSheet, Text, View } from "react-native";
 
 
 export default function CharacterCard({item}) {
   return (
     <View>
+        <Image source={{ uri: item.image }} style={styles.image} resizeMode="contain" />
         <Text>{item.name}</Text>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+    image: {
+      width: 80,
+      height: 80
+    },
+    row: {
+      flex: 1,
+      flexDirection: "row",
+      margin: 10
+    },
+    column: {
+      flex: 1,
+      flexDirection: "column",
+      justifyContent: "flex-start"
+    },
+    text: {
+      fontSize: 18
+    }
+  });
