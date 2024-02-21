47aade9261b2665063b46210c4414a7cc33e8aa4
Sufi <116170704+SufiAzarquiel@users.noreply.github.com>
2024-02-21 10:54:53 +0100

    ghgfh

--- src/screens/MundosScreen.jsx
+++ src/screens/MundosScreen.jsx
@@ -1,9 +1,42 @@
-import { Text, View } from "react-native";
+import { useEffect, useState } from "react";
+import { View, Text, FlatList } from "react-native";
+import { getPlanetsByPage } from "../services/dragonBallAPI";
+
 
 export default function MundosScreen() {
+  const [mundos, setMundos] = useState([])
+  const [paginaActual, setPaginaActual] = useState(1)
+  const [paginasTotales, setPaginasTotales] = useState(0)
+
+  const getMundos = (page=1)=>{
+    getPlanetsByPage(page)
+    .then(json=>{
+      setMundos(previos=>[...previos, ...json.items])
+      setPaginasTotales(json.meta.totalPages)
+      setPaginaActual(json.meta.currentPage)
+    })
+    .catch(error=>console.log(error))
+  }
+
+  useEffect(() => {
+    getMundos();
+  }, []);
+
   return (
     <View>
-        <Text>Mundos</Text>
+      <FlatList
+        data={mundos}
+        renderItem={({ item }) => (
+          <Text key={item.id}>{item.name}</Text>
+        )}
+        onEndReachedThreshold={0}
+        onEndReached={() => {
+          if (paginaActual < paginasTotales) {
+            getMundos(paginaActual + 1);
+            setPaginaActual(paginaActual + 1);
+          }
+        }}
+      />
     </View>
-  )
-}
+  );
+};
--- src/services/dragonBallAPI.js
+++ src/services/dragonBallAPI.js
@@ -5,3 +5,15 @@ export async function getCharactersByPage(page = 1) {
   const json = await data.json();
   return json;
 }
+
+export async function getPlanetsByPage(page) {
+  let pageStr = ""
+  if(page === 1) {
+    pageStr = ""
+  } else {
+    pageStr = "?page=2&limit=10"
+  }
+  const data = await fetch(`${API}planets${pageStr}`);
+  const json = await data.json();
+  return json;
+}
