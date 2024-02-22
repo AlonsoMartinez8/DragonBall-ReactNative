9ae653a711ccd503b96c5076663619093aa72a1e
AlonsoMartinez8 <alumno.574777@ies-azarquiel.es>
2024-02-22 11:57:55 +0100

    MundoDetails design & Create PersonajeFromMundoCard component

---
+++ src/components/PersonajeFromMundoCard.jsx
@@ -0,0 +1,7 @@
+import { Text } from 'react-native'
+
+export default function PersonajeFromMundoCard({personaje}) {
+  return (
+    <Text>{personaje.name}</Text>
+  )
+}
--- src/screens/MundoDetails.jsx
+++ src/screens/MundoDetails.jsx
@@ -1,37 +1,91 @@
-import React from 'react'
-import { View, Text, Image, StyleSheet } from 'react-native'
+import React from "react";
+import {
+  View,
+  Text,
+  Image,
+  StyleSheet,
+  ImageBackground,
+  ScrollView,
+} from "react-native";
 
 export default function CharacterDetails({ route }) {
   const { item } = route.params;
-  return (
-    <View style={styles.column}>
-      <Image source={{ uri: item.image }} style={styles.image} resizeMode="contain" />
-      <View style={[styles.column, { marginLeft: 10 }]}>
-        <Text style={[styles.text, { fontWeight: "bold" }]}>{item.name}</Text>
-        <Text style={styles.text}>{item.description}</Text>
-        
+  const mundoDestruido = (destruido) => {
+    if (destruido) {
+      return "Destruido";
+    } else {
+      return "Sin destruir";
+    }
+  };
 
-      </View>
-    </View>
-  )
+  return (
+    <ImageBackground source={{ uri: item.image }} style={styles.container}>
+      <ScrollView style={styles.scrollview}>
+        <View style={styles.colCenter}>
+          <Text style={styles.name}>{item.name}</Text>
+        </View>
+        <View style={styles.colStart}>
+          <View style={styles.colStart}>
+            <Text style={styles.label}>Descripción</Text>
+            <Text style={styles.text}>{item.description}</Text>
+          </View>
+          <View style={styles.colStart}>
+            <Text style={styles.label}>Destruido</Text>
+            <Text style={styles.text}>{mundoDestruido(item.isDestroyed)}</Text>
+          </View>
+          <View style={styles.colStart}>
+            <Text style={styles.label}>Personajes</Text>
+            <Text style={styles.text}>{item.gender}</Text>
+          </View>
+        </View>
+      </ScrollView>
+    </ImageBackground>
+  );
 }
 
 const styles = StyleSheet.create({
   image: {
     width: 300,
-    height: 300
+    height: 300,
   },
-  row: {
-    flex: 1,
-    flexDirection: "row",
-    margin: 10
-  },
-  column: {
-    flex: 1,
+  colCenter: {
+    display: "flex",
     flexDirection: "column",
-    margin: 20
+    alignItems: "center",
+    padding: 20,
+  },
+  colStart: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "start",
+  },
+  scrollview: {
+    height: "100%",
+    paddingHorizontal: 20,
+  },
+  label: {
+    display: "flex",
+    fontWeight: "700",
+    fontSize: 15,
+    textAlign: "center",
+    marginTop: 10,
+    backgroundColor: "#ddd",
   },
   text: {
-    fontSize: 18
-  }
+    textAlign: "center",
+    backgroundColor: "#1119",
+    padding: 5,
+    color: "#fff",
+  },
+  name: {
+    fontSize: 30,
+    color: "#fff",
+    fontWeight: "800",
+    borderWidth: 1,
+    borderColor: "#fff",
+    paddingVertical: 10,
+    paddingHorizontal: 40,
+    backgroundColor: "#1113",
+    marginTop: 10,
+  },
 });
